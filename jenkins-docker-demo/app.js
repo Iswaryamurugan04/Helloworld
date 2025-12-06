74ebc60b335f9301ba882579a381c97f4daa6c6d
Iswarya <iswaryaishu2000@gmail.com>
2025-12-06 17:03:39 +0530

    changes in docker file

--- jenkins-docker-demo/app.js
+++ jenkins-docker-demo/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello from Jenkins + Docker!'));
+app.get('/', (req, res) => res.send('Hi from Jenkins + Docker!'));
 app.listen(3000, () => console.log('App running on port 3000'));
